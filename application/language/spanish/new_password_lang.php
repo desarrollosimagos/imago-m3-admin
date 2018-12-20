9bb2099a3b04b095977a6959c539d0e3863ab786
Jesus Laya <jesusgerard2008@gmail.com>
2018-12-20 12:31:02 -0400

    IMP Declarado variables de idiomas

---
+++ application/language/spanish/new_password_lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['new_passwd_placeholder_user'] = 'ejemplo@ejemplo.com';
+$lang['new_passwd_button_value'] = 'Enviar';
+$lang['new_passwd_question_account'] = '¿Desea volver?';
+$lang['new_passwd_login_account'] = 'Iniciar sesión';
