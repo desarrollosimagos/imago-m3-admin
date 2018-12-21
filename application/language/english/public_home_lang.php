d82b0e443de721b72174ba232d08dbefe6f3b347
Jesus Laya <jesusgerard2008@gmail.com>
2018-12-21 12:10:22 -0400

    IMP - Cambiar el saludo

--- application/language/english/public_home_lang.php
+++ application/language/english/public_home_lang.php
@@ -3,6 +3,7 @@
 /* PÁGINA PÚBLICA DE INICIO */
 
 // Contenido de publicidad
+$lang['welcome_home'] = 'Bienvenido a OCTOPUS.';
 $lang['public_home_content_title1'] = 'INVEST IN COMS';
 $lang['public_home_content_title2'] = 'WITH A GUARANTEE OF SUCCESS';
 $lang['public_home_content_subtitle1'] = 'Transparency and high profitability in the short term';
