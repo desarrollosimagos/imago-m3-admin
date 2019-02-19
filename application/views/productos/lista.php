cbbcc31580c62bdcbc9607672a9b3c26aa5fea13
Jesus Laya <jesusgerard2008@gmail.com>
2019-02-19 14:56:04 -0400

    IMP - Aplicado estilo css para aumentar el archo de las celdas desplegable

--- application/views/productos/lista.php
+++ application/views/productos/lista.php
@@ -2,6 +2,13 @@
 .select2-container {
 	z-index: 99999;
 }
+table.dataTable tr.child ul li {
+    border-bottom: 1px solid #efefef;
+    padding: 0.5em 0;
+        padding-top: 0.5em;
+    white-space: nowrap;
+    width: 850% !important;
+}
 </style>
 <div class="row wrapper border-bottom white-bg page-heading">
     <div class="col-lg-10">
